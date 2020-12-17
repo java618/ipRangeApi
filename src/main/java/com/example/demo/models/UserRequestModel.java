384c87ffdfa7c91b59a8a83d27dd938cab84ef80
java618 <sikdir287>
2020-12-17 15:23:23 +0400

    new updates

---
+++ src/main/java/com/example/demo/models/UserRequestModel.java
@@ -0,0 +1,25 @@
+package com.example.demo.models;
+
+public class UserRequestModel {
+    private String name;
+    private String password;
+
+    public UserRequestModel() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
